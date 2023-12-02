c839ac73821872e0868da60c5f125367fbf0fc63
Parth Ghinaiya <parth@gopronto.io>
2023-12-02 13:17:30 +0530

    REFACTOR data collector pipeline to functionalize a code

--- project/data_collector.py
+++ project/data_collector.py
@@ -34,34 +34,101 @@ DATASET_DICT = {
     },
 }
 
-# Instantiate the Kaggle API
-kaggle_api = KaggleApi()
 
-# Authenticate using the API key
-kaggle_api.authenticate()
+def authenticate_kaggle():
+    """
+    authenticate kaggle
+    api credentials are stored in os env
+    :return:
+    """
+    kaggle_api = KaggleApi()
+    kaggle_api.authenticate()
 
-# Download a datasets in current directory and unzip it
-for key, val in DATASET_DICT.items():
-    kaggle_api.dataset_download_files(val['dataset_path'], path='./', unzip=True)
+    return kaggle_api
 
-# Read the CSV file into a Pandas DataFrame
-crude_oil_df = pd.read_csv(DATASET_DICT["CRUDE_OIL_DATASET"]["file_name"])
-gold_price_df = pd.read_csv(DATASET_DICT["GOLD_PRICE_DATASET"]["file_name"])
 
-# preprocess the crude-oil dataset
-crude_oil_df['date'] = pd.to_datetime(crude_oil_df['date']).dt.date
+def download_dataset(kaggle_api):
+    """
+    Download a datasets in current directory and unzip it
+    :param kaggle_api:
+    :return:
+    """
+    for key, val in DATASET_DICT.items():
+        kaggle_api.dataset_download_files(val['dataset_path'], path='./', unzip=True)
 
-# insert crude oil data into sqlite database
-crud_oil_db_engine = create_engine(f"sqlite:///data/{DATASET_DICT['CRUDE_OIL_DATASET']['database_name']}.sqlite")
-crude_oil_df.to_sql(DATASET_DICT['CRUDE_OIL_DATASET']['database_name'], crud_oil_db_engine, index=False,
-                    if_exists='replace',
-                    dtype=DATASET_DICT['CRUDE_OIL_DATASET']['sqlalchemy_datatype'])
+    return
 
-# preprocess the crude-oil dataset
-gold_price_df['Date'] = pd.to_datetime(gold_price_df['Date'], format='%d-%m-%Y')
 
-# insert gold price data into sqlite database
-gold_price_db_engine = create_engine(f"sqlite:///data/{DATASET_DICT['GOLD_PRICE_DATASET']['database_name']}.sqlite")
-gold_price_df.to_sql(DATASET_DICT['GOLD_PRICE_DATASET']['database_name'], gold_price_db_engine, index=False,
-                     if_exists='replace',
-                     dtype=DATASET_DICT['GOLD_PRICE_DATASET']['sqlalchemy_datatype'])
+def get_and_preprocess_crude_oil_dataframe():
+    """
+    create crude oil dataframe and preprocess it from csv
+    :param:
+    :return:
+    """
+    # create crude oil dataframe from csv
+    crude_oil_df = pd.read_csv(DATASET_DICT["CRUDE_OIL_DATASET"]["file_name"])
+
+    # preprocess the crude-oil dataset
+    crude_oil_df['date'] = pd.to_datetime(crude_oil_df['date']).dt.date
+
+    return crude_oil_df
+
+
+def get_and_preprocess_gold_price_dataframe():
+    """
+    create gold price dataframe and preprocess it from csv
+    :param:
+    :return:
+    """
+    # create crude oil dataframe from csv
+    gold_price_df = pd.read_csv(DATASET_DICT["GOLD_PRICE_DATASET"]["file_name"])
+
+    # preprocess the crude-oil dataset
+    gold_price_df['Date'] = pd.to_datetime(gold_price_df['Date'], format='%d-%m-%Y')
+
+    return gold_price_df
+
+
+def dump_dataset_to_db(dataframe, db_name, datatype):
+    """
+    insert data into sqlite database
+    :param dataframe:
+    :param db_name:
+    :param datatype:
+    :return:
+    """
+
+    db_engine = create_engine(f"sqlite:///data/{db_name}.sqlite")
+    dataframe.to_sql(db_name, db_engine, index=False, if_exists='replace', dtype=datatype)
+    return
+
+
+def data_collector():
+    """
+    collect data from kaggle and store it into local sqlite database with appropriate datatype
+    :return:
+    """
+    kaggle_api = authenticate_kaggle()
+
+    # download dataset in local storage
+    download_dataset(kaggle_api)
+
+    # create crude oil dataframe from csv
+    crude_oil_df = get_and_preprocess_crude_oil_dataframe()
+
+    # create crude oil dataframe from csv
+    gold_price_df = get_and_preprocess_gold_price_dataframe()
+
+    # insert crude oil data into sqlite database
+    dump_dataset_to_db(dataframe=crude_oil_df, db_name=DATASET_DICT['CRUDE_OIL_DATASET']['database_name'],
+                       datatype=DATASET_DICT['CRUDE_OIL_DATASET']['sqlalchemy_datatype'])
+
+    # insert gold price data into sqlite database
+    dump_dataset_to_db(dataframe=gold_price_df, db_name=DATASET_DICT['GOLD_PRICE_DATASET']['database_name'],
+                       datatype=DATASET_DICT['GOLD_PRICE_DATASET']['sqlalchemy_datatype'])
+
+    return
+
+
+if __name__ == "__main__":
+    data_collector()
